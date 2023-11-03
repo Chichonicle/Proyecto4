cf1c81f9e59f5d4de0abcd8532c8a6588aafea36
Christian <christianperez.rbb@gmail.com>
2023-11-03 01:22:25 +0100

    fix: bug with .env fixed.

--- src/db.ts
+++ src/db.ts
@@ -5,6 +5,7 @@ import { User1698858773796 } from "./migration/1698858773796-user"
 import { Worker1698966212313 } from "./migration/1698966212313-worker"
 import { Appointment1698967851480 } from "./migration/1698967851480-appointment"
 import { worker } from "./models/worker"
+import "dotenv/config"
 
 
 
@@ -12,7 +13,7 @@ import { worker } from "./models/worker"
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT as string),
+    port: 3306,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
